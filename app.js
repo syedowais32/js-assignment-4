89b14cc04ef1e9c134975037da5528a18497e4e3
syedgulzar86 <syedgulzarmohammad123@gmail.com>
2024-03-20 15:49:22 +0500

    assignment done

---
+++ app.js
@@ -0,0 +1,41 @@
+// chapter 14-16
+// var arr = [] ;  
+// let sting = ["asking", "verifying", "qeustioning"];
+// let num = [ 1, 2, 3, 4, 5 ];
+// let buliean = [ true ,false ]; 
+// let misedUp = [ "asking", 2, "verifying", 3, true ];
+// let educate = [ "SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD" ];
+// document.write ( <h3>Qualifications :</h3> 1) ${educate[0]} <br> 2) ${educate[1]} <br> 3) ${educate[2]} <br> 4) ${educate[3]} <br> 5) ${educate[4]} <br> 6) ${educate[5]} <br> 7) ${educate[6]} <br> 8) ${educate[7]} <br> );
+
+// let studentName = ["bawa", "gulzar" ,"anwer"] , studentScore = [40 , 50 , 60] , totalScore = [80] , per = [studentScore[0] / totalScore[0] * 100 , studentScore[1] / totalScore[0] * 100 , studentScore[2] / totalScore[0] * 100];
+
+// document.write(score of bawa is ${studentScore[0]} percentage:${per[0]} <br> score of gulzar is ${studentScore[1]}. percentage:${per[1]} <br> score of anwer is ${studentScore[2]}. percentage:${per[2]});
+
+// let colorName = ["red" , "blue" , "black" , "pink" , "purple" , "green"];// ,  prom = prompt("add your fav color begin") ,  prom1 = prompt("add your fav color end") , prom2 = prompt("add your fav color at your fav place"), prom3 = +prompt("which place add you want to add your first color", "starting place as 0" ); 
+
+
+// let  prom3 = +prompt("(red, blue, black, pink, purple, green) From which place-number U want to remove the color from the list", "starting place as 0" ) 
+// , prom4 = prompt("(red, blue, black, pink, purple, green) How many colors U want to delete", "starting place as 1");
+// colorName.unshift(prom);
+// colorName.unshift("grey" , "orange");
+// colorName.shift();
+// colorName.push(prom1);
+
+// colorName.splice(prom3 , 0, prom2);
+
+// colorName.splice(prom3 , prom4,);
+
+
+// document.write(colorName)   ;
+
+// let arrScore = [4522 ,2022, 5022 , 4022 ,2250];
+// arrScore.sort();
+// document.write(arrScore);
+
+// let cities = ["karachi" , "lahore" , "islamabad" , "quetta" , "peshawer"];
+// let selected = cities.slice(1 , 4);
+// document.write(selected);
+
+// var arr = ["this", "is", "my", "cat"];
+// arr.toString();
+// document.write(arr)
